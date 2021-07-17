df3204e25058238ff0f49343ab5f248517ea2e07
nxp915 <nxp915@student.bham.ac.uk>
2021-07-17 22:09:56 +0100

    Cleaned code, removed unnecessary comments, changed text on button

--- src/main/java/org/nic/App.java
+++ src/main/java/org/nic/App.java
@@ -27,75 +27,49 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
 
-
-        //File folder = new File("/Users/nicolaspoffley/Desktop/pics");
-        //String[] filesStrings = folder.list();
-
         try {
 
-            // set title for the stage
             stage.setTitle("FileChooser");
-
-            // create a File chooser
             DirectoryChooser directoryChooser = new DirectoryChooser();
+            Label label = new Label("Image Corruptor");
+            Button button = new Button("Choose Directory Of Images");
 
-            // create a Label
-            Label label = new Label("no files selected");
-
-            // create a Button
-            Button button = new Button("Show open dialog");
-
-            // create an Event Handler
             EventHandler<ActionEvent> event =
-                    new EventHandler<>() {
+                    e -> {
 
-                        public void handle(ActionEvent e) {
+                        File selectedDirectory = directoryChooser.showDialog(stage);
 
-                            // get the file selected
-                            File selectedDirectory = directoryChooser.showDialog(stage);
+                        if (selectedDirectory != null) {
 
-                            if (selectedDirectory != null) {
+                            System.out.println(selectedDirectory.getAbsolutePath());
+                            File folder = new File(selectedDirectory.getAbsolutePath());
+                            String[] filesStrings = folder.list();
 
-                                System.out.println(selectedDirectory.getAbsolutePath());
-                                File folder = new File(selectedDirectory.getAbsolutePath());
-                                String[] filesStrings = folder.list();
-
-
-
-                                CorruptorLogic edge = new CorruptorLogic();
-                                for (int i = 0; i < filesStrings.length; i++) {
-                                    if (filesStrings[i].endsWith("png") || filesStrings[i].endsWith("jpeg") || filesStrings[i].endsWith("jpg")) {
-                                        Image image = null;
-                                        try {
-                                            image = new Image(new FileInputStream("/Users/nicolaspoffley/Desktop/pics/" + filesStrings[i]));
-                                        } catch (FileNotFoundException f) {
-                                            f.printStackTrace();
-                                        }
-                                        Color[][] img = (edge.filterImage(image));
-                                        System.out.println(filesStrings[i]);
-                                        edge.saveImage(img, "/Users/nicolaspoffley/Desktop/OUT/" + filesStrings[i]);
-                                        System.out.println("Saved image " + i + "/" + (filesStrings.length - 1));
+                            CorruptorLogic edge = new CorruptorLogic();
+                            for (int i = 0; i < filesStrings.length; i++) {
+                                if (filesStrings[i].endsWith("png") || filesStrings[i].endsWith("jpeg") || filesStrings[i].endsWith("jpg")) {
+                                    Image image = null;
+                                    try {
+                                        image = new Image(new FileInputStream("/Users/nicolaspoffley/Desktop/pics/" + filesStrings[i]));
+                                    } catch (FileNotFoundException f) {
+                                        f.printStackTrace();
                                     }
+                                    Color[][] img = (edge.filterImage(image));
+                                    System.out.println(filesStrings[i]);
+                                    edge.saveImage(img, "/Users/nicolaspoffley/Desktop/OUT/" + filesStrings[i]);
+                                    System.out.println("Saved image " + i + "/" + (filesStrings.length - 1));
                                 }
-
-
                             }
+
+
                         }
                     };
 
             button.setOnAction(event);
 
-
-            // create a VBox
-            VBox vbox = new VBox(30, label, button);
-
-            // set Alignment
+            VBox vbox = new VBox(60, label, button);
             vbox.setAlignment(Pos.CENTER);
-
-            // create a scene
             Scene scene = new Scene(vbox, 800, 500);
-
-            // set the scene
             stage.setScene(scene);
 
             stage.show();
